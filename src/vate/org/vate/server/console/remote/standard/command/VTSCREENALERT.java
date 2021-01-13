04ca80ade3f5c73fb9afe087d5e1141b06ecd673
wknishio <wknishio@gmail.com>
2021-01-13 14:52:03 -0300

    updated *VTSCREENALERT syntax

--- src/vate/org/vate/server/console/remote/standard/command/VTSCREENALERT.java
+++ src/vate/org/vate/server/console/remote/standard/command/VTSCREENALERT.java
@@ -14,8 +14,8 @@ public class VTSCREENALERT extends VTServerStandardRemoteConsoleCommandProcessor
 	{
 		this.setFullName("*VTSCREENALERT");
 		this.setAbbreviatedName("*VTSCA");
-		this.setFullSyntax("*VTSCREENALERT <[TITLE/]ALERT> [DISPLAY]");
-		this.setAbbreviatedSyntax("*VTSCA <[TI/]ALT> [DP]");
+		this.setFullSyntax("*VTSCREENALERT <ALERT> [[TITLE] [DISPLAY]]");
+		this.setAbbreviatedSyntax("*VTSCA <ALT> [[TI] [DP]]");
 	}
 
 	public void execute(String command, String[] parsed) throws Exception
@@ -24,24 +24,27 @@ public class VTSCREENALERT extends VTServerStandardRemoteConsoleCommandProcessor
 		{
 			if (parsed.length == 2)
 			{
+				String alert = parsed[1];
 				// splitCommand[1] =
 				// StringEscapeUtils.unescapeJava(splitCommand[1]);
-				if (parsed[1].indexOf('/') < 0)
-				{
-					VTGraphicsMessager.showAlert(VTConsole.getFrame(), "Variable-Terminal Server", parsed[1]);
-					session.getServer().displayTrayIconMessage("Variable-Terminal Server", "["+ parsed[1] + "]");
-				}
-				else
-				{
-					String title = parsed[1].substring(0, parsed[1].indexOf('/'));
-					String message = parsed[1].substring(parsed[1].indexOf('/') + 1);
-					VTGraphicsMessager.showAlert(VTConsole.getFrame(), title, message);
-					session.getServer().displayTrayIconMessage(title, "[" + message + "]");
-				}
+//				if (parsed[1].indexOf('/') < 0)
+//				{
+//					VTGraphicsMessager.showAlert(VTConsole.getFrame(), "Variable-Terminal Server", parsed[1]);
+//					session.getServer().displayTrayIconMessage("Variable-Terminal Server", "["+ parsed[1] + "]");
+//				}
+//				else
+//				{
+//					String title = parsed[1].substring(0, parsed[1].indexOf('/'));
+//					String message = parsed[1].substring(parsed[1].indexOf('/') + 1);
+//					VTGraphicsMessager.showAlert(VTConsole.getFrame(), title, message);
+//					session.getServer().displayTrayIconMessage(title, "[" + message + "]");
+//				}
+				VTGraphicsMessager.showAlert(VTConsole.getFrame(), "Variable-Terminal Server", alert);
+				session.getServer().displayTrayIconMessage("Variable-Terminal Server", "["+ alert + "]");
 				connection.getResultWriter().write("\nVT>Graphical alert sent to server!\nVT>");
 				connection.getResultWriter().flush();
 			}
-			else if (parsed.length >= 3)
+			else if (parsed.length == 3)
 			{
 				try
 				{
@@ -49,26 +52,31 @@ public class VTSCREENALERT extends VTServerStandardRemoteConsoleCommandProcessor
 					// StringEscapeUtils.unescapeJava(splitCommand[1]);
 					// splitCommand[2] =
 					// StringEscapeUtils.unescapeJava(splitCommand[2]);
-					final GraphicsDevice[] devices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+					String alert = parsed[1];
 					int number = Integer.parseInt(parsed[2]);
+					final GraphicsDevice[] devices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
 					if (number < devices.length)
 					{
 						if (number <= -1)
 						{
 							number = 0;
 						}
-						if (parsed[1].indexOf('/') < 0)
-						{
-							VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), "Variable-Terminal Server", parsed[1]);
-							session.getServer().displayTrayIconMessage("Variable-Terminal Server", "[" + parsed[1] + "]");
-						}
-						else
-						{
-							String title = parsed[1].substring(0, parsed[1].indexOf('/'));
-							String message = parsed[1].substring(parsed[1].indexOf('/') + 1);
-							VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), title, message);
-							session.getServer().displayTrayIconMessage(title, "[" + message + "]");
-						}
+//						if (parsed[1].indexOf('/') < 0)
+//						{
+//							VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), "Variable-Terminal Server", parsed[1]);
+//							session.getServer().displayTrayIconMessage("Variable-Terminal Server", "[" + parsed[1] + "]");
+//						}
+//						else
+//						{
+//							String title = parsed[1].substring(0, parsed[1].indexOf('/'));
+//							String message = parsed[1].substring(parsed[1].indexOf('/') + 1);
+//							VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), title, message);
+//							session.getServer().displayTrayIconMessage(title, "[" + message + "]");
+//						}
+						VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), "Variable-Terminal Server", alert);
+						session.getServer().displayTrayIconMessage("Variable-Terminal Server", "[" + alert + "]");
+						connection.getResultWriter().write("\nVT>Graphical alert sent to server!\nVT>");
+						connection.getResultWriter().flush();
 					}
 					else
 					{
@@ -77,6 +85,48 @@ public class VTSCREENALERT extends VTServerStandardRemoteConsoleCommandProcessor
 					}
 				}
 				catch (NumberFormatException e)
+				{
+					//final GraphicsDevice[] devices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+					String alert = parsed[1];
+					String title = parsed[2];
+					VTGraphicsMessager.showAlert(VTConsole.getFrame(), title, alert);
+					session.getServer().displayTrayIconMessage(title, "[" + alert + "]");
+					connection.getResultWriter().write("\nVT>Graphical alert sent to server!\nVT>");
+					connection.getResultWriter().flush();
+					//connection.getResultWriter().write("\nVT>Graphical display device not found!\nVT>");
+					//connection.getResultWriter().flush();
+				}
+			}
+			else if (parsed.length >= 4)
+			{
+				String alert = parsed[1];
+				String title = parsed[2];
+				int number = Integer.parseInt(parsed[3]);
+				final GraphicsDevice[] devices = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices();
+				if (number < devices.length)
+				{
+					if (number <= -1)
+					{
+						number = 0;
+					}
+//					if (parsed[1].indexOf('/') < 0)
+//					{
+//						VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), "Variable-Terminal Server", parsed[1]);
+//						session.getServer().displayTrayIconMessage("Variable-Terminal Server", "[" + parsed[1] + "]");
+//					}
+//					else
+//					{
+//						String title = parsed[1].substring(0, parsed[1].indexOf('/'));
+//						String message = parsed[1].substring(parsed[1].indexOf('/') + 1);
+//						VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), title, message);
+//						session.getServer().displayTrayIconMessage(title, "[" + message + "]");
+//					}
+					VTGraphicsMessager.showAlert(devices[number], VTConsole.getFrame(), title, alert);
+					session.getServer().displayTrayIconMessage(title, "[" + alert + "]");
+					connection.getResultWriter().write("\nVT>Graphical alert sent to server!\nVT>");
+					connection.getResultWriter().flush();
+				}
+				else
 				{
 					connection.getResultWriter().write("\nVT>Graphical display device not found!\nVT>");
 					connection.getResultWriter().flush();
