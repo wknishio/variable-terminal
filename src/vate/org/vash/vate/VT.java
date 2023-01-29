e16a7dfb95de2214899ec81dbb5d337e0e64fd2f
wknishio <wknishio@gmail.com>
2023-01-29 12:31:55 -0300

    increased buffer sizes to 64KB

--- src/vate/org/vash/vate/VT.java
+++ src/vate/org/vash/vate/VT.java
@@ -31,10 +31,10 @@ public class VT
 
   public static final int VT_NETWORK_PACKET_HEADER_SIZE = 8;
   public static final int VT_NETWORK_PACKET_SIZE = 1024 * 4;
-  public static final int VT_NETWORK_PACKET_BUFFER_SIZE = 1024 * 32;
-  public static final int VT_STANDARD_DATA_BUFFER_SIZE = 1024 * 32;
-  public static final int VT_COMPRESSED_DATA_BUFFER_SIZE = 1024 * 32;
-  public static final int VT_FILE_DATA_BUFFER_SIZE = 1024 * 32;
+  public static final int VT_NETWORK_PACKET_BUFFER_SIZE = 1024 * 64;
+  public static final int VT_STANDARD_DATA_BUFFER_SIZE = 1024 * 64;
+  public static final int VT_COMPRESSED_DATA_BUFFER_SIZE = 1024 * 64;
+  public static final int VT_FILE_DATA_BUFFER_SIZE = 1024 * 64;
   public static final int VT_SMALL_DATA_BUFFER_SIZE = 1024 * 16;
 
   public static final int VT_MULTIPLEXED_CHANNEL_TYPE_PIPED = 0;
