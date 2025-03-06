a90be2ebd359023a00195ab46d3acf1cada0a32f
wknishio <wknishio@gmail.com>
2025-03-06 17:08:52 -0300

    reduced packet buffer size

--- src/vate/org/vash/vate/VT.java
+++ src/vate/org/vash/vate/VT.java
@@ -31,7 +31,7 @@ public class VT
   public static final int VT_PACKET_DATA_SIZE_BYTES = 1024 * 16;
   public static final int VT_PACKET_TOTAL_SIZE_BYTES = VT_PACKET_HEADER_SIZE_BYTES + VT_PACKET_DATA_SIZE_BYTES;
   
-  public static final int VT_CONNECTION_INPUT_PACKET_BUFFER_SIZE_BYTES = 1024 * 1024 * 8;
+  public static final int VT_CONNECTION_INPUT_PACKET_BUFFER_SIZE_BYTES = 1024 * 1024 * 4;
   public static final int VT_CONNECTION_OUTPUT_PACKET_BUFFER_SIZE_BYTES = 1024 * 64;
   public static final int VT_CHANNEL_PACKET_BUFFER_SIZE_BYTES = 1024 * 256;
   public static final int VT_STANDARD_BUFFER_SIZE_BYTES = 1024 * 64;
