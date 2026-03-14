89889c2d53abc64c2f22e295c309f1b8f211d06e
wknishio <wknishio@gmail.com>
2026-03-14 08:57:23 -0300

    added VTNullOutputStream class

---
+++ src/vate/org/vash/vate/stream/limit/VTNullOutputStream.java
@@ -0,0 +1,37 @@
+package org.vash.vate.stream.limit;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public final class VTNullOutputStream extends OutputStream
+{
+  public VTNullOutputStream()
+  {
+    super();
+  }
+  
+  public final void write(final int b) throws IOException
+  {
+    
+  }
+  
+  public final void write(final byte[] b) throws IOException
+  {
+    
+  }
+  
+  public final void write(final byte[] b, final int off, final int len) throws IOException
+  {
+    
+  }
+  
+  public final void flush() throws IOException
+  {
+    
+  }
+  
+  public final void close() throws IOException
+  {
+    
+  }
+}
