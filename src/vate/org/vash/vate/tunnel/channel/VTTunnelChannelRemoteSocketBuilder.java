1c7fa37448a8ded5850aec4ef7e58d3b397aaa58
wknishio <wknishio@gmail.com>
2026-09-18 13:34:36 -0300

    updated VTTunnelChannelRemoteSocketBuilder class

--- src/vate/org/vash/vate/tunnel/channel/VTTunnelChannelRemoteSocketBuilder.java
+++ src/vate/org/vash/vate/tunnel/channel/VTTunnelChannelRemoteSocketBuilder.java
@@ -6,7 +6,6 @@ import java.net.DatagramSocket;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-import org.vash.vate.VTSystem;
 import org.vash.vate.proxy.client.VTProxy;
 import org.vash.vate.proxy.client.VTProxy.VTProxyType;
 import org.vash.vate.stream.multiplex.VTMultiplexingInputStream.VTMultiplexedInputStream;
@@ -460,7 +459,7 @@ public class VTTunnelChannelRemoteSocketBuilder
   
   public Socket pipeSocket(boolean request, String bind, int type) throws IOException
   {
-    type &= ~(VTSystem.VT_MULTIPLEXED_CHANNEL_TYPE_PIPE_DIRECT);
+    //type &= ~(VTSystem.VT_MULTIPLEXED_CHANNEL_TYPE_PIPE_DIRECT);
     if (bind == null)
     {
       bind = "";
@@ -564,7 +563,7 @@ public class VTTunnelChannelRemoteSocketBuilder
   
   public Socket pipeSocket(boolean request, String bind, int type, OutputStream out) throws IOException
   {
-    type &= ~(VTSystem.VT_MULTIPLEXED_CHANNEL_TYPE_PIPE_DIRECT);
+    //type &= ~(VTSystem.VT_MULTIPLEXED_CHANNEL_TYPE_PIPE_DIRECT);
     if (bind == null)
     {
       bind = "";
@@ -603,7 +602,10 @@ public class VTTunnelChannelRemoteSocketBuilder
         {
           pipedSocket = (VTTunnelPipedSocket) handler.getSession().getSocket();
         }
-        handler.getSession().getTunnelInputStream().setOutputStream(out, pipedSocket);
+        if (out != null)
+        {
+          handler.getSession().getTunnelInputStream().setOutputStream(out, pipedSocket);
+        }
         handler.getSession().getTunnelOutputStream().open();
         handler.getSession().getTunnelInputStream().ready();
       }
@@ -629,7 +631,11 @@ public class VTTunnelChannelRemoteSocketBuilder
       session.setSocketOutputStream(pipedSocket.getOutputStream());
       
       input.close();
-      input.setOutputStream(out, pipedSocket);
+      if (out != null)
+      {
+        input.setOutputStream(out, pipedSocket);
+      }
+      
       //output.open();
       
       session.setTunnelInputStream(input);
